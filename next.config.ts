29a2b5305b41ec2260e8a07e7131042362e246bf
Nicolas Toscano <nicolas.toscano@gmail.com>
2025-03-21 14:54:03 -0700

    updated nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  distDir: '.next',
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 };
 
 export default nextConfig;
